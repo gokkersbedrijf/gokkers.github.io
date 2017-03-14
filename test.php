ec4cfd67fdc21e4019a527c62ec0e7d53cee70ab
ratje44 <cornesierat@gmail.com>
2017-03-14 13:29:14 +0100

    first commit
    includes test file

---
+++ test.php
@@ -0,0 +1,22 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Corne
+ * Date: 14-3-2017
+ * Time: 13:26
+ */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1> hello world</h1>
+<p> hosted by github pages</p>
+
+</body>
+</html>
